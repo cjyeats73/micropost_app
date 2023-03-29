195ccff4b6ff16d4820560ef0417414aa2752636
cjyeats <coltenyeats@gmail.com>
2023-03-29 09:29:40 -0400

    bug fixes idk

--- test/controllers/menu_pages_controller_test.rb
+++ test/controllers/menu_pages_controller_test.rb
@@ -4,10 +4,19 @@ class MenuPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get Home" do
     get menu_pages_Home_url
     assert_response :success
+    assert_select "title"
   end
 
   test "should get Help" do
     get menu_pages_Help_url
     assert_response :success
+    assert_select "title"
   end
+
+  test "should get About" do
+    get menu_pages_About_url
+    assert_response :success
+    assert_select "title"
+  end
+
 end
