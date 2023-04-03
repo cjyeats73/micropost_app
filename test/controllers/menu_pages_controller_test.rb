60f8b308a703221ba517ce62c331d4ade9e9eae1
cjyeats <coltenyeats@gmail.com>
2023-04-03 09:46:28 -0400

    more more testing fixes

--- test/controllers/menu_pages_controller_test.rb
+++ test/controllers/menu_pages_controller_test.rb
@@ -4,19 +4,19 @@ class MenuPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get Home" do
     get menu_pages_Home_url
     assert_response :success
-    assert_select "title"
+    assert_select "title", "Home | My Micropost App"
   end
 
   test "should get Help" do
     get menu_pages_Help_url
     assert_response :success
-    assert_select "title"
+    assert_select "title", "Help | My Micropost App"
   end
 
   test "should get About" do
     get menu_pages_About_url
     assert_response :success
-    assert_select "title"
+    assert_select "title", "About | My Micropost App"
   end
 
 end
