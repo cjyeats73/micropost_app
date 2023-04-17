7263f936c0cfee0d591205317a826550f38ca550
cjyeats <coltenyeats@gmail.com>
2023-04-17 10:03:02 -0400

    overhaul update

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,2 @@
 class Category < ApplicationRecord
-    validates :name, length: {in: 3..25}
-    validates :name, uniqueness: true
-    has_one :name
-    validates :name, presence: true 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :microposts
   resources :users
   get 'menu_pages/Home'
