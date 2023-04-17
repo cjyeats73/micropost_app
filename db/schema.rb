752a787c740c18762ebc4b899f46581add5be19b
cjyeats <coltenyeats@gmail.com>
2023-04-17 10:13:43 -0400

    some bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_001748) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_140035) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "microposts", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
