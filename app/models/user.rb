13ceaee8f3e6da99aa15bae5478c81d0c7a23d96
cjyeats <coltenyeats@gmail.com>
2023-04-21 09:48:52 -0400

    micropost partial update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 50}
-    has_many :microposts
+    has_many :microposts, dependent: :destroy
     VALID_EMAIL_REGEX = /\A[\w\.\+\-]*\w@[\w\-]+\.[a-zA-Z]+\z/i
     validates :email, format: {with: VALID_EMAIL_REGEX}
 end
