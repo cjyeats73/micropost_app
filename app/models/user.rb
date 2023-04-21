0bf0f6e1071644d095aa644c6c569c1707f3edb3
cjyeats <coltenyeats@gmail.com>
2023-04-21 09:18:38 -0400

    foster family grouping update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-VALID_EMAIL_REGEX = /\A[\w\.\+\-]*\w@[\w\-]+\.[a-zA-Z]+\z/i
-validates :email, format: {with: VALID_EMAIL_REGEX}
+    validates :name, presence: true, length: {maximum: 50}
+    has_many :microposts
+    VALID_EMAIL_REGEX = /\A[\w\.\+\-]*\w@[\w\-]+\.[a-zA-Z]+\z/i
+    validates :email, format: {with: VALID_EMAIL_REGEX}
 end
