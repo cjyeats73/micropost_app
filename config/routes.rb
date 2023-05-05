d02ec699d33933af9f23f0f6cff7670d29ade8f7
cjyeats <coltenyeats@gmail.com>
2023-05-05 10:39:40 -0400

    M2M view update

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
