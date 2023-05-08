ca44d378951527ff4b7bb958e3bf6b185b24c485
cjyeats <coltenyeats@gmail.com>
2023-05-08 10:02:10 -0400

    Adding to views M2M update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
     has_many :followers, through: :followed_relationships, source: :follower
     VALID_EMAIL_REGEX = /\A[\w\.\+\-]*\w@[\w\-]+\.[a-zA-Z]+\z/i
     validates :email, format: {with: VALID_EMAIL_REGEX}
+
+    # Follows a user.
+    def follow(other_user) 
+        
+    following << other_user unless self == other_user
+
+    end 
+    # Unfollows a user.
+    def unfollow(other_user)
+        following.delete(other_user)
+    end 
+    # Returns true if the current user is following the other user. 
+    def following?(other_user)
+        following.include?(other_user)
+    end 
 end
